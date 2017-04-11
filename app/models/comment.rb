9180d3a3e29b2f7b56be35f6750075f3a75b0c11
Rodrigo Saffie <rsaffie@gmail.com>
2017-04-10 22:22:18 -0300

    Add comments validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,7 @@
+# frozen_string_literal: true
+
+##
+# Model for comments
 class Comment < ApplicationRecord
+  validates :content, presence: true, allow_blank: false
 end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20170411011946) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.text     "content"
+    t.text     "content",    null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
