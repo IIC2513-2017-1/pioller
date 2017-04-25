83f53fa9fce421a5bb063da7b7f281ea577baeaa
Rodrigo Saffie <rsaffie@gmail.com>
2017-04-25 00:16:57 -0300

    Add login and logout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@
 # Controller for landing page
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  helper_method :current_user
+
+  protected
+
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = session.key?(:user_id) && User.find(session[:user_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :comments
   resources :tweets
   resources :users
+  resource :session, only: [:new, :create, :destroy]
   root 'home#index'
 end
