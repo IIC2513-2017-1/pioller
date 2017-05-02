d15c1b1d0a341fed61a583d592591d11c9065d14
Rodrigo Saffie <rsaffie@gmail.com>
2017-05-02 00:04:41 -0300

    Keep users' passwords secure

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 ##
 # Model for users
 class User < ApplicationRecord
+  has_secure_password
+
   validates :password, presence: true, length: { minimum: 6 },
                        confirmation: true, allow_blank: false
   validates :email, presence: true, uniqueness: true, allow_blank: false,
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170413145632) do
+ActiveRecord::Schema.define(version: 20170427130818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,12 +53,12 @@ ActiveRecord::Schema.define(version: 20170413145632) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",      null: false
+    t.string   "email",           null: false
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "password",   null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
