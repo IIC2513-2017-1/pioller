60c3a5def5f13c82a072bf9b0962a9f2b490e7df
Rodrigo Saffie <rsaffie@gmail.com>
2017-05-10 20:29:25 -0300

    Assign current_user as owner of new tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -87,6 +87,6 @@ class TweetsController < ApplicationController
   # Never trust parameters from the scary internet, only allow the white
   # list through.
   def tweet_params
-    params.require(:tweet).permit(:content, :private, :user_id)
+    params.require(:tweet).permit(:content, :private).merge(user_id: current_user.id)
   end
 end
